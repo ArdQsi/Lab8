ea01c612a550c8c1bebfd9066a15096c24d79221
Roman <roman_kravets_den@mail.ru>
2022-05-17 16:26:21 +0300

    Начальная прога

---
+++ server/src/main/java/collection/CollectionManager.java
@@ -0,0 +1,40 @@
+package collection;
+
+import data.Product;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface CollectionManager<T> {
+
+    void sort();
+
+    Product getById(long id);
+
+    void add(T element);
+
+    String getInfo();
+
+    Collection<T> getCollection();
+
+    boolean chekID(long id);
+
+    void updateById(long id, Product newProduct);
+
+    void removeById(long id);
+
+    void clear();
+
+    void addIfMin(Product product);
+
+    void removeGreater(Collection<Long> ids);
+
+    List<Product> filterLessThanManufactureCost(Float cost);
+
+    List<String> getUniqueOwner();
+
+    void removeLower(Collection<Long> ids);
+
+    void deserializeCollection();
+
+}
